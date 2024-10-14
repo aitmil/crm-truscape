ebd46488ba00ca442a8d9295ebc007685a6611f9
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-14 22:27:44 +0300

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import AddCompanyButton from './components/add-company-button';
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </div>
   );
 }
