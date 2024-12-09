3249fb9dc4c36bf5f3175e3d4f8feb78729d3b85
Anna Taranchuk <taranchuk4@yahoo.com>
2024-12-09 18:20:59 +0200

    add dashboard slots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import AddCompanyButton from './components/add-company-button';
-import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       <AddCompanyButton />
     </div>
   );
